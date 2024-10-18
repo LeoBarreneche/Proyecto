4552a1c3a643d808212ad20b9c3f7320b12cd5d2
46824125 <46824125@10.41.10.252>
2024-10-18 20:39:33 -0300

    subida inicial

---
+++ src/pruebaanube/PruebaANube.java
@@ -0,0 +1,20 @@
+
+package pruebaanube;
+
+import java.util.*;
+
+
+public class PruebaANube {
+
+    
+    public static void main(String[] args) {
+        ArrayList <Integer> ci = new ArrayList <Integer>();
+        
+        ci.add(46824125);
+        
+        System.out.println("Cedula ingresada: "+ ci.get(0));
+       
+        
+    }
+    
+}
